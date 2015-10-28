2f1796a5addc6f099efb7a00d8f81d884a75d82b
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-10-28 19:36:00 +0100

    Packagify

--- zone-nyan.el
+++ zone-nyan.el
@@ -1,3 +1,40 @@
+;;; zone-nyan.el --- Zone out with nyan cat
+
+;; Copyright (C) 2015 Vasilij Schneidermann <v.schneidermann@gmail.com>
+
+;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
+;; URL: https://github.com/wasamasa/zone-nyan
+;; Version: 0.0.1
+;; Package-Requires: ((esxml "0.3.1"))
+;; Keywords: zone
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING. If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; A graphical zone program displaying the infamous nyan cat
+;; animation.  Requires a graphical Emacs instance with SVG support.
+
+;; See the README for more info:
+;; https://github.com/wasamasa/zone-nyan
+
+;;; Code:
+
 (require 'esxml)
 
 
@@ -541,3 +578,7 @@
   (interactive)
   (let ((zone-programs [zone-nyan]))
     (zone)))
+
+(provide 'zone-nyan)
+
+;;; zone-nyan.el ends here
